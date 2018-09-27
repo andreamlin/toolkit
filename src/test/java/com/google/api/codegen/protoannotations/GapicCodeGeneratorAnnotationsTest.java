22e604591ce72238d691560d83933e1ec360ed59
Andrea Lin <asdf.lina@gmail.com>
2018-09-26 17:34:13 -0700

    add in everything from baselines_annotations

--- src/test/java/com/google/api/codegen/protoannotations/GapicCodeGeneratorAnnotationsTest.java
+++ src/test/java/com/google/api/codegen/protoannotations/GapicCodeGeneratorAnnotationsTest.java
@@ -14,10 +14,13 @@
  */
 package com.google.api.codegen.protoannotations;
 
+import static com.google.api.codegen.ArtifactType.LEGACY_GAPIC_AND_PACKAGE;
+
+import com.google.api.codegen.ArtifactType;
 import com.google.api.codegen.CodegenTestUtil;
 import com.google.api.codegen.common.TargetLanguage;
 import com.google.api.codegen.gapic.GapicTestBase2;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +33,10 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class GapicCodeGeneratorAnnotationsTest extends GapicTestBase2 {
 
+  // TODO(andrealin): Change this to GAPIC_CODE when proto annotations are fully supported and
+  // yaml files are no longer required.
+  private static final ArtifactType ARTIFACT_TYPE = LEGACY_GAPIC_AND_PACKAGE;
+
   private final String apiName;
 
   public GapicCodeGeneratorAnnotationsTest(
@@ -40,18 +47,37 @@ public class GapicCodeGeneratorAnnotationsTest extends GapicTestBase2 {
       String apiName,
       String baseline,
       String protoPackage) {
-    //    super(language, null, null, snippetName, baseline);
-    super(language, null, null, snippetName, baseline, protoPackage);
+    super(
+        language, gapicConfigFileNames, packageConfigFileName, snippetName, baseline, protoPackage);
 
     this.apiName = apiName;
+    // Use the library.proto contained in this test package's testdata.
     getTestDataLocator().addTestDataSource(getClass(), "testdata");
+
+    // Use the common yaml files from the codegen test package's testsrc/common.
     getTestDataLocator().addTestDataSource(CodegenTestUtil.class, "testsrc/common");
+    // TODO(andrealin): Remove dependency on yaml files when proto annotations fully supported.
   }
 
   @Parameters(name = "{3}")
   public static List<Object[]> testedConfigs() {
-    return new LinkedList<>();
-    // TODO(andrealin): Implement parsing proto-annotations, make baseline files, write tests.
+    return Arrays.asList();
+    // TODO(andrealin): Add these back in after baselines are checked in.
+    // return Arrays.asList(
+    //     GapicTestBase2.createTestConfig(
+    //         TargetLanguage.GO,
+    //         new String[] {"library_gapic.yaml"},
+    //         null,
+    //         "library",
+    //         "google.example.library.v1",
+    //         ARTIFACT_TYPE),
+    //     GapicTestBase2.createTestConfig(
+    //         TargetLanguage.JAVA,
+    //         new String[] {"library_gapic.yaml"},
+    //         "library_pkg2.yaml",
+    //         "library",
+    //         "google.example.library.v1",
+    //         ARTIFACT_TYPE));
   }
 
   @Test
